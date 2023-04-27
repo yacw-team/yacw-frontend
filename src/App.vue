fe0c2504db8f561da372d4f5e68c8606c4f96998
Kare-Udon <gpg@udon.eu.org>
2023-04-27 09:08:34 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+  <RouterLink to="/about" class="p-2 bg-gray-300 border-black border-2">About</RouterLink>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,4 @@
+<template>
+  <el-button> Element-plus test button</el-button>
+  <button class="bg-red-300">TailwindCSS test button</button>
+</template>
