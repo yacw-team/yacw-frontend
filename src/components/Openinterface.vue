d5169fa1d67ec3a56201cdcc99413ebaf2b3d07f
Kare-Udon <52364111+Kare-Udon@users.noreply.github.com>
2023-05-03 15:09:11 +0000

    Add: Create character component

---
+++ src/components/Openinterface.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-button @click="showModal = true">AIcharacter</el-button>
+    <Modal :show="showModal" :component-name="componentName" @close="showModal = false"></Modal>
+  </div>
+</template>
+
+<script>
+import Modal from './Modal.vue'
+import AIcharacter from './AIcharacter.vue'
+
+export default {
+  name: "Open",
+  components: {
+    Modal,
+    AIcharacter,
+  },
+  data() {
+    return {
+      showModal: false,
+      componentName: 'AIcharacter',
+    }
+  }
+};
+</script>
