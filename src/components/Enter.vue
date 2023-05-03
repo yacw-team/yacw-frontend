f72b1317a9163679916bb2d6d2feb00848f85545
soft-lipapa <106940531+soft-lipapa@users.noreply.github.com>
2023-05-04 00:21:13 +0800

    Create Enter.vue

---
+++ src/components/Enter.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-button text @click="open">Enter License Key</el-button>
+  </template>
+  
+  <script lang="ts" setup>
+  import { ElMessage, ElMessageBox } from 'element-plus'
+  
+  const open = () => {
+    ElMessageBox.prompt('Please Enter your License Key ', 'Enter License Key', {
+      confirmButtonText: 'OK',
+      cancelButtonText: 'Cancel',
+      inputPattern:
+      /^[a-zA-Z0-9]+$/,
+      inputErrorMessage: 'Invalid Key',
+    })
+      .then(({ value }) => {
+        ElMessage({
+          type: 'success',
+          message: `Your Key is:${value}`,
+        })
+      })
+      .catch(() => {
+        ElMessage({
+          type: 'info',
+          message: 'Input canceled',
+        })
+      })
+  }
+  </script>
+  
