c2c655959697cbd61f3f44da46017b80447a0e60
Kare-Udon <gpg@udon.eu.org>
2023-05-04 21:01:22 +0800

    Add: emotionList & styleList

---
+++ src/views/Translate/list/emotion.ts
@@ -0,0 +1,42 @@
+export const emotionList = [
+    {
+        value: 'happiness and pleasure',
+        label: 'Joyful'
+    },
+    {
+        value: 'frustration or irritation',
+        label: 'Angry'
+    },
+    {
+        value: 'apprehension or anxiety',
+        label: 'Fearful'
+    },
+    {
+        value: 'sorrow or melancholy',
+        label: 'Sad'
+    },
+    {
+        value: 'excitement or eagerness',
+        label: 'Enthusiastic'
+    },
+    {
+        value: 'self-assurance or conviction',
+        label: 'Confident'
+    },
+    {
+        value: 'amazement or astonishment',
+        label: 'Surprised'
+    },
+    {
+        value: 'revulsion or distaste',
+        label: 'Disgusted'
+    },
+    {
+        value: 'optimism or positive expectations',
+        label: 'Hopeful'
+    },
+    {
+        value: 'deep thoughtfulness or contemplation',
+        label: 'Pensive'
+    },
+];
