b920fd7b02e3f15e5dc75db16ed8db13f383a6bc
saltyyfish <72616866+saltyyfish@users.noreply.github.com>
2023-05-05 10:25:06 +0800

    Update AIcharacter.vue

--- src/components/AIcharacter.vue
+++ src/components/AIcharacter.vue
@@ -1,10 +1,9 @@
 <template>
   <div style="width:1000px;
-  padding-left:100px;">
+        padding-left:100px;">
    
-   <div style="width:500px;
-   padding:10px 100px ;" 
-   >
+   <div style = "width:500px;
+   padding:10px 100px;" >
    <el-form label-position="top">
       <el-form-item > 
         <el-input v-model="searchText" @input="handleSearch"  
@@ -19,12 +18,12 @@
     <el-button  @click="createCharacter">Add Character</el-button>
     </div>
     <el-row :gutter="35">
-      <el-col style="padding:20px 80px  ;" :span="12" v-for="(character, index) in filteredCharacters" :key="index">
+      <el-col style="padding:20px 80px  ;" :span="12" v-for="(personalities, index) in filteredPersonalities" :key="index">
         <el-card  shadow="hover" style="width:350px; padding: 20px; border-radius:25px; ">
         <UserFilled style="width: 1em; height: 1em; margin-right: 8px" />
           <div style="height: 150px; ;overflow-y: auto;">
-            <h3>{{ character.name }}</h3>
-            <p>{{ character.introduction }}</p>
+            <h3>{{ personalities.ModelName }}</h3>
+            <p>{{ personalities.Details }}</p>
           </div>
         </el-card>
       </el-col>
@@ -35,56 +34,63 @@
 </template>
 <script setup lang="ts">
 import { defineProps, ref } from 'vue';
+import axios from "axios";
 
-interface Character {
-  name: string;
-  introduction: string;
+interface Personality {
+ 
+	Id       : number
+	ModelName :string
+	Details   :string
+	Prompt    :string
+	Uid       :string
+	Designer  :number
 }
 
+
 const props = defineProps({
-  characters: {
-    type: Array as () => Character[],
+  personalities: {
+    type: Array as () => Personality[],
     required: true,
     default() {
       return [
         {
-          name: 'Career Counselor',
-          introduction:
+          ModelName: 'Career Counselor',
+          Details:
             'A career counselor who can help you with anything related to your career from job searching to career planning.',
         },
         {
-          name: 'Financial Advisor',
-          introduction:
+          ModelName: 'Financial Advisor',
+          Details:
             'A financial advisor who can provide advice on budgeting, saving, investing, retirement planning, and other financial issues.',
         },
         {
-          name: 'Personal Trainer',
-          introduction:
+          ModelName: 'Personal Trainer',
+          Details:
             'A personal trainer who can help you stay fit and improve your health by creating personalized fitness plans and providing workout routines.',
         },
         {
-          name: 'Software Developer',
-          introduction:
+          ModelName: 'Software Developer',
+          Details:
             'A software developer who can help you develop software programs and applications using programming languages such as Java, Python, and JavaScript.',
         },
         {
-          name: 'Life Hacker',
-          introduction:
+          ModelName: 'Life Hacker',
+          Details:
             'A life hacker who can provide useful tips and tricks to help you optimize your daily routines, save time, and increase productivity.',
         },
         {
-          name: 'DIY Expert',
-          introduction:
+          ModelName: 'DIY Expert',
+          Details:
             'A DIY expert who can provide advice and guidance on do-it-yourself projects, such as woodworking, painting, and crafting.',
         },
         {
-          name: 'Mindfulness Coach',
-          introduction:
+          ModelName: 'Mindfulness Coach',
+          Details:
             'A mindfulness coach who can guide you through meditation and relaxation techniques to help you focus on the present moment and reduce stress and anxiety.',
         },
         {
-          name: 'Journalist',
-          introduction:
+          ModelName: 'Journalist',
+          Details:
             'A journalist who can research and report on newsworthy events, conduct interviews, and write news articles and stories.',
         },
       ];
@@ -93,19 +99,20 @@ const props = defineProps({
 });
 
 const searchText = ref('');
-const filteredCharacters = ref<Character[]>(props.characters);
+const filteredPersonalities = ref<Personality[]>(props.personalities);
 
-function handleSearch() {
-  filteredCharacters.value = props.characters.filter((character) =>
-    character.name.toLowerCase().includes(searchText.value.toLowerCase())
+async function handleSearch() {
+  const response = await axios.get('');
+  const personalities: Personality[] = response.data;
+  filteredPersonalities.value = personalities.filter((personality) =>
+    personality.ModelName.toLowerCase().includes(searchText.value.toLowerCase())
   );
 }
 
-function createCharacter() {
+function createPersonality() {
   // 这里可以使用API调用或其他方式实现创建角色的逻辑
-  console.log('创建角色');
+  console.log('创建Personality');
 }
-
 </script>
 
 <style scoped>
@@ -123,3 +130,5 @@ p{
     color:grey;
 }
 </style>
+
+
