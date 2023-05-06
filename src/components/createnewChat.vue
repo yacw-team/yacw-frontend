401d22fc6bbd572b2586d68e9d42d00f60ddfce4
fmfjjjjkk <853723049@qq.com>
2023-05-06 22:12:59 +0800

    add createnewchat and update sidebar

---
+++ src/components/createnewChat.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    
+      <span @click="click1">{{ icon1 }}</span>
+      <span>{{conversation.title}}</span>
+      <span @click="click2">{{ icon2 }}</span>
+      <el-dialog v-model="dialogVisible1">
+        <p class="item">更改标题</p>
+        <el-input v-model="input" :placeholder= "conversation.title"/>
+        <template #footer>
+          <el-button class="item2" @click="closedialog">确定</el-button>
+        </template>
+      </el-dialog>
+   
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, defineProps,PropType } from "vue";
+const icon1 = "📄";
+const icon2 = "✍️";
+
+const input = ref("");
+function click1() {
+  alert(111);
+}
+
+const dialogVisible1 = ref(false);
+
+function click2() {
+
+  dialogVisible1.value = true;
+}
+
+
+function closedialog() {
+  if(conversation.title!= input.value)
+  {
+    conversation.updateItems(conversation.index,input.value)  
+  }
+  dialogVisible1.value = false;
+}
+
+const conversation = defineProps({
+  title:String,
+  chatID: String,
+  index:Number,
+
+  updateItems: {
+    type: Function as PropType<(index:any, newTitle: string) => void>,
+    required: true,
+  },
+
+});
+
+
+</script>
+
+<style scoped>
+span {
+  padding: 2%;
+}
+span:hover {
+  background-color: yellow;
+}
+.item {
+  font-size: large;
+  display: flex;
+  justify-content: center;
+}
+.item2 {
+  margin-right: 10px;
+}
+</style>
