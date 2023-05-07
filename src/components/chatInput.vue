5f3dbac75f4f2380922fc2572f1722dec07d81ee
soft-lipapa <106940531+soft-lipapa@users.noreply.github.com>
2023-05-07 17:23:19 +0800

    Update chatInput.vue
    PromptLibrary can use directory and refresh immediately

--- src/components/chatInput.vue
+++ src/components/chatInput.vue
@@ -2,20 +2,20 @@
     <div>
         <h2>Test2</h2>
         <!-- <input type="text" v-model="jsonData.prompts"> -->
-        <textarea type="text" v-model="input"></textarea>
+        <textarea type="text" v-model="input" ></textarea>
        
     </div>
     <PromptLibrary></PromptLibrary>
 </template>
   
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue';
+import { computed, ref, onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import PromptLibrary from '@/components/PromptLibrary.vue'
 
 
 const route = useRoute();
-
+const refresh=ref(true);
 //也可以获取整个的prompt
 // const queryString = decodeURIComponent(route.query.encodedData);
 //const jsonData = JSON.parse(queryString);
@@ -25,22 +25,43 @@ const route = useRoute();
 
 
 
-const Jsondata = route.query.data;
+const Jsondata = route.query.data?route.query.data:null;
 const prompts = Jsondata ? JSON.parse(Jsondata) : "";
+const temp=ref<string>(prompts);
 const input = computed({
     get() {
-        return localStorage.getItem('temp')+prompts;
+        
+        return localStorage.getItem('temp')+temp.value;
     },
     set(value) {
         localStorage.setItem('temp', value);
     }
 });
 
+onMounted(()=>[
+    input.value
+])
+
+watch(() => route.query.data, (newVal) => {
+  const data = newVal ? JSON.parse(newVal) : '';
+  temp.value = data;
+});
 
 
-onMounted(() => {
-  
-})
+// 解析URL中的数据
+// const urlParams = new URLSearchParams(window.location.search);
+// let data = urlParams.get('data') || "";
+
+// // 使用ref创建响应式变量
+// const dataInput = ref(data);
+
+// // 监听data变化
+// watch(urlParams, (newVal) => {
+//   const newData = newVal.get("data") || "";
+//   dataInput.value = newData;
+// });
+
+
 
 </script>
   
