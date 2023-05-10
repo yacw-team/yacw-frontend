e454138cf4bde9c4d220e7da675731bc603b5f6d
soft-lipapa <106940531+soft-lipapa@users.noreply.github.com>
2023-05-10 19:34:52 +0800

    Update PromptLibrary.vue

--- src/components/PromptLibrary.vue
+++ src/components/PromptLibrary.vue
@@ -1,55 +1,85 @@
 <template>
-  <div>
-    <el-button @click="centerDialogVisible = true">Click to open the Dialog</el-button>
-
-    <el-dialog v-model="centerDialogVisible" title="Prompt Library" width="30%" align-center center>
-      <el-row :gutter="50">
-        <el-col :span="18">
-          <el-input v-model="input" placeholder="Search your library" />
-        </el-col>
-        <el-col :span="5">
-          <el-button>确定</el-button>
-        </el-col>
-      </el-row>
-      <br />
-      <el-row :gutter="50">
-        <el-col :span="12">
-          <el-text>
-            Get more prompt please visit
-          </el-text>
-          <button @click="changeComponent" style="color: rgb(122, 127, 212);">
-            prompt shop</button>
-        </el-col>
+  <el-button @click="centerDialogVisible = true">Click to open the Dialog</el-button>
+  <el-dialog v-model="centerDialogVisible" title="Prompt Library" align-center center
+    :style="{ 'max-width': '30%', 'max-height': '80%' }" width="auto" height="auto">
+    <el-tabs v-model="activeName" class="demo-tabs">
+      <el-tab-pane label="Your prompt" name="first">
+        <el-row :gutter="50">
+          <el-col :span="16">
+            <el-input v-model="input" placeholder="Search your prompt" />
+          </el-col>
+          <el-col :span="6">
+            <el-button>确定</el-button>
+          </el-col>
+        </el-row>
+        <br />
         <el-col :span="12">
           <el-button @click="toggle">新建prompt</el-button>
           <Cp v-if="createdialog" />
         </el-col>
-      </el-row>
-      <br />
-      <el-row v-for="index in p" :key="index" :gutter="50" class="item">
-        <el-col :span="24">
-          <el-card shadow="always">
-            <span>prompt名字: {{ index.name }}</span>
-            <br />
-            <span>prompt内容: {{ index.content }}</span>
-            <br />
-            <el-button @click="getdetails(index.details)">Use it</el-button>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-dialog>
-  </div>
+        <br />
+        <el-row v-for="index in p" :key="index" :gutter="50" class="item">
+          <el-col :span="24">
+            <el-card shadow="always">
+              <span>prompt名字: {{ index.name }}</span>
+              <br />
+              <span>prompt内容: {{ index.content }}</span>
+              <br />
+              <div style="display:flex;justify-content:flex-end;align-items:flex-end;">
+                <el-button @click="getdetails(index.details)">Use it</el-button>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="Community Prompts" name="second">
+        <el-row :gutter="50">
+          <el-col :span="16">
+            <el-input v-model="input" placeholder="Search your prompt" />
+          </el-col>
+          <el-col :span="5">
+            <el-button>确定</el-button>
+          </el-col>
+        </el-row>
+        <br />
+        <el-row :gutter="50">
+          <el-col :span="18">
+            <el-text>
+              Get more prompt please visit
+              <button @click="changeComponent" style="color: rgb(122, 127, 212);">
+                prompt shop</button>
+            </el-text>
+          </el-col>
+        </el-row>
+        <br />
+        <el-row v-for="index in p" :key="index" :gutter="50" class="item">
+          <el-col :span="24">
+            <el-card shadow="always">
+              <span>prompt名字: {{ index.name }}</span>
+              <br />
+              <span>prompt内容: {{ index.content }}</span>
+              <br />
+              <div style="display:flex;justify-content:flex-end;align-items:flex-end;">
+                <el-button @click="getdetails(index.details)">Use it</el-button>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+
+      </el-tab-pane>
+    </el-tabs>
+  </el-dialog>
 </template>
+
 <script lang="ts" setup>
 import { inject, onMounted, ref } from "vue";
 import Cp from "./CreatePersonality.vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
-import promptShop from '@/components/PromptShop.vue'
-
-//后期与刘少对接
 const centerDialogVisible = ref(false);
 const createdialog = ref(false);
+const activeName = ref('first')
+
 function toggle() {
   createdialog.value = !createdialog.value;
 }
@@ -62,25 +92,45 @@ interface Prompt {
   exampleOutput: string;
   content: string;
 }
+
+//获取后端的公开的prompt
+const fetchprompts = async (): Promise<Prompt[]> => {
+  const response = await axios.get<Prompt[]>("API/v1/chat/prompts");
+  return response.data;
+};
+
+//获取后端的用户的prompt
+const fetchUserprompts = async (): Promise<Prompt[]> => {
+  const responseUerPrompt = await axios.get<Prompt[]>("API/v1/chat/prompts");
+  return responseUerPrompt.data;
+};
+   
+
 onMounted(() => {
-  const fetchprompts = async (): Promise<Prompt[]> => {
-    const response = await axios.get<Prompt[]>("API/v1/chat/prompts");
-    return response.data;
-  };
   (async () => {
     type prompts = Array<Prompt>;
     // eslint-disable-next-line no-redeclare
     let prompts = await fetchprompts();
     console.log(prompts);
   })();
+
+  (async () => {
+    type userPrompts = Array<Prompt>;
+    // eslint-disable-next-line no-redeclare
+    let userPrompts = await fetchprompts();
+    console.log(userPrompts);
+  })();
 });
+
+
+
 type p = Array<Prompt>;
 const p = [
   {
     id: "1",
     name: "2",
     description: "33",
-    details: "44;",
+    details: "软件工程师;",
     exampleInput: "55;",
     exampleOutput: "66;",
     content: "77;",
@@ -89,7 +139,7 @@ const p = [
     id: "1",
     name: "234",
     description: "33",
-    details: "44;",
+    details: "音乐家;",
     exampleInput: "55;",
     exampleOutput: "626;",
     content: "77;",
