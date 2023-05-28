451745f045732cf52df49d228de6462128f8aa0d
Kare-Udon <gpg@udon.eu.org>
2023-05-28 20:28:21 +0800

    Update: 提取 HomePage Axios 函数 #115

---
+++ src/api/home/home.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+import type { checkAPIKeyReq } from "./req";
+
+const URLs = {
+    CheckAPIKey: "/api/v1/home/checkAPIKey",
+};
+
+export async function checkAPIKey(APIKey: string): Promise<string> {
+    const req: checkAPIKeyReq = {
+        apiKey: APIKey,
+    };
+    axios
+        .post(URLs.CheckAPIKey, req)
+        .then(async (response) => {
+            return response.data.status;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    return "";
+}
