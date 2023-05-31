4ba724be56a6ce7c8d553e34506085597daaaa38
Soft-lipa <106940531+soft-lipapa@users.noreply.github.com>
2023-05-31 22:40:55 +0800

    Update TopBarMenu.vue

--- src/views/Game/components/TopBarMenu.vue
+++ src/views/Game/components/TopBarMenu.vue
@@ -1,39 +1,25 @@
 <template>
-    <div class="flex flex-col space-y-4 ...">
-        <div>
-            <el-menu
-              :default-active="activeIndex"
-              class="el-menu-demo"
-              mode="horizontal"
-              :ellipsis="false"
-              @select="handleSelect"
-            >
-              <el-menu-item index="0">YACW</el-menu-item>
-              <div class="flex-grow" />
-              <el-menu-item index="1" text @click="dialogTableVisible = true">帮助</el-menu-item>
-              <el-dialog v-model="dialogTableVisible" title="游戏玩法">
-                <el-text type="textarea"></el-text>
-              </el-dialog>
-    
-              <el-sub-menu index="2">
-                <template #title>查看历史</template>
-                <el-menu-item index="2-1">item one</el-menu-item>
-                <el-menu-item index="2-2">item two</el-menu-item>
-                <el-menu-item index="2-3">item three</el-menu-item>
-                <el-menu-item index="2-3">查看全部历史</el-menu-item>
-              </el-sub-menu>
-              <el-menu-item index="3">退出</el-menu-item>
-            </el-menu>
-        </div>
-    </div>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false" @select="handleSelect">
+    <el-menu-item index="0">YACW</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1" text @click="dialogTableVisible = true">帮助</el-menu-item>
+    <el-dialog v-model="dialogTableVisible" title="游戏玩法">
+      <el-text type="textarea"></el-text>
+    </el-dialog>
+    <el-menu-item index="3" @click="showGlobalComponent">退出</el-menu-item>
+  </el-menu>
 </template>
 
 
 <script setup lang="ts">
 
-import { ref } from 'vue'
+import { ref, inject, type Ref } from 'vue'
 const dialogTableVisible = ref(false)
+const shouldShowGlobalComponent = inject('shouldShowGlobalComponent') as Ref<boolean>
 
+function showGlobalComponent() {
+  shouldShowGlobalComponent.value = true
+}
 
 const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
@@ -43,7 +29,6 @@ const handleSelect = (key: string, keyPath: string[]) => {
 </script>
 
 <style>
-
 .flex-grow {
   flex-grow: 1;
 }
@@ -51,12 +36,15 @@ const handleSelect = (key: string, keyPath: string[]) => {
 .el-button--text {
   margin-right: 15px;
 }
+
 .el-select {
   width: 300px;
 }
+
 .el-input {
   width: 300px;
 }
+
 .dialog-footer button:first-child {
   margin-right: 10px;
 }
