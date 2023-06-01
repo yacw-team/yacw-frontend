f4186fc1bba9beba83f6ac7af1b504b5fd50d0e1
Soft-lipa <106940531+soft-lipapa@users.noreply.github.com>
2023-06-01 10:21:14 +0800

    Update prompt.ts

--- src/api/chat/prompt/prompt.ts
+++ src/api/chat/prompt/prompt.ts
@@ -1,19 +1,18 @@
-import type { sendNewPrompt } from "@/api/chat/prompt/req";
-import type { Prompt } from "@/api/chat/prompt/res"
-import axios from "axios"
+import type { sendNewPrompt,myPromptApikey,sendPromptId } from "@/api/chat/prompt/req";
+import type {Prompt} from "@/api/chat/prompt/res"
+import axios  from "axios"
 
-const URL = {
-    Getmyprompts: "/api/v1/chat/myprompts",
-    Getprompts: "/api/v1/chat/prompts",
+const URL ={
+    Getmyprompts:"/api/v1/chat/myprompts",
+    Getprompts:"/api/v1/chat/prompts",
+    Deleteprompt:"/api/v1/chat/deleteprompts"
 }
 
-export async function getmyprompts(req: string): Promise<Prompt[]> {
-    let resData: Prompt[] = [];
+export async function getmyprompts(req:myPromptApikey): Promise<Prompt[]> {
+    let resData: Prompt[] =[];
     try {
         const response = await axios.post(URL.Getmyprompts, req)
-
-        resData = response.data.Prompts;
-
+        resData = response.data.Prompts;      
     } catch (error) {
         console.error(error);
         throw new Error('Failed to get message');
@@ -21,12 +20,11 @@ export async function getmyprompts(req: string): Promise<Prompt[]> {
     return resData;
 }
 
-export async function getprompts(): Promise<Prompt[]> {
-    let resData: Prompt[] = [];
+export async function getprompts(): Promise<Prompt[] > {
+    let resData: Prompt[] =[];
     try {
         const response = await axios.get(URL.Getprompts)
-
-        resData = response.data.Prompts;
+        resData = response.data.Prompts;        
     } catch (error) {
         console.error(error);
         throw new Error('Failed to get message');
@@ -34,33 +32,29 @@ export async function getprompts(): Promise<Prompt[]> {
     return resData;
 }
 
-export async function getPromptsByType(type: string) {
-    let reqURL = "";
-    if (type != null) {
-        reqURL = URL.Getprompts + "?type=" + type;
-    } else {
-        reqURL = URL.Getprompts;
-    }
-    let resData: Prompt[] = [];
-    try {
-        const response = await axios.get(reqURL)
-        resData = response.data.Prompts;
-    } catch (error) {
-        console.error(error);
-        throw new Error('Failed to get message');
-    }
-    return resData;
-}
-
-export async function getnewprompts(req: sendNewPrompt): Promise<Prompt> {
+export async function getnewprompts(req:sendNewPrompt): Promise<Prompt> {
     let resData: Prompt;
     try {
-        const response = await axios.post(URL.Getprompts, req)
-        resData = response.data;
+        const response = await axios.post(URL.Getprompts,req)
+        resData = response.data;        
     } catch (error) {
         console.error(error);
         throw new Error('Failed to get message');
     }
     return resData;
 }
+
+export async function deleteprompts(req:sendPromptId): Promise<Prompt> {
+    let resData: Prompt;
+    try {
+        const response = await axios.post(URL.Deleteprompt,req)
+        resData = response.data;        
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to get message');
+    }
+    return resData;
+}
+
+
 
